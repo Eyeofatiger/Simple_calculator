bb38d74e7560a841e4f8dcb9739997fc186bf9cc
Oleh <eyeofatiger@yandex.ua>
2019-09-24 18:25:32 +0100

    Working calculator

--- js/Jscript.js
+++ js/Jscript.js
@@ -1,1 +1,23 @@
 "use strict";
+
+function insert(num){
+    document.form.textView.value = document.form.textView.value + num;
+}
+
+function clean(){
+    document.form.textView.value = "";
+}
+
+function back(){
+    let exp = document.form.textView.value;
+
+    document.form.textView.value = exp.substring(0, exp.length-1);
+}
+
+function equal(){
+    let exp = document.form.textView.value;
+
+    if(exp){
+        document.form.textView.value = eval(exp);
+    }
+}
